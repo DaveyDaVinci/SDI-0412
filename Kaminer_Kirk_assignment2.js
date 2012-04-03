27365c5739bb1268fa7393d66c123cfb4721fe6e
LULZwes <wkreply@gmail.com>
2012-04-03 14:40:23 -0400

    added new js file

---
+++ Kaminer_Kirk_assignment2.js
@@ -0,0 +1,48 @@
+// Author: Kirk Kaminer
+// 3/28/2012
+// Deliverable 1
+// The not so proud of Challenge.
+
+alert("Project 2!");
+
+var character = "Druid",
+	strong = "Tanking",
+	firstaid = "Healing",
+	dare = "Challenge",
+	iAmSeventy = true,
+	keepPlaying = false,
+	daysPlayed = 4,
+	totalLevels = 70;
+;	
+
+var nspoc = character + " starts at level one. " +
+	strong + " allows him to take on multiple mobs. " +
+	firstaid + " ables him to heal self and others. " + "this is a great " + dare + ".";
+;
+	console.log(nspoc);
+
+var classIPicked = "Will the " + character + " reach level 70 " + 
+    "in four days playtime completing the " + dare + "?";
+;
+	console.log(classIPicked);
+
+var reaching70 = "Now that the Druid is level 70, will he play on to level 85 or get some much needed rest?";
+;
+	console.log(reaching70);
+
+if (iAmSeventy == true){}
+
+if (iAmSeventy){}
+	if (character = totalLevels) {
+		console.log( "He now sleeps off the much needed rest." )
+}	else {
+		console.log("The " + character + " remains at level 70 on the servers to never be played again." )
+}
+
+if (keepPlaying == false){}
+if (daysPlayed === totalLevels){
+		console.log(character + "played for " + daysPlayed + " day, to reach level " + totalLevels + ".")
+}	else {
+		console.log("the " + dare + " was accomplished, and the " + character + " will remain level " 		+ totalLevels + " for the rest of his days.")
+}
+
