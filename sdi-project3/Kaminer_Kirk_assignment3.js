e697737ea87575033b116eec8ba11828d51b8a38
Wes Kaminer <WesK@static.local>
2012-04-18 00:43:56 -0400

    updated project3 JS

--- sdi-project3/Kaminer_Kirk_assignment3.js
+++ sdi-project3/Kaminer_Kirk_assignment3.js
@@ -1,1 +1,16 @@
-alert("JavaScript works!");
+// Author: Kirk Kaminer
+// 04/18/2012
+// Assignment 3
+// JSON
+
+alert("Project 3!");
+
+var handleData = function (group){
+	for (var i = 0; i < group.characters.length; i++){
+		var character = group.characters[i];
+		console.log("Class: " + character.player +", Role: " + character.role + ", Level: " +
+		character.level);
+	};
+};
+
+handleData(group2);
