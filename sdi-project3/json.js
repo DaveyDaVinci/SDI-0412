aeb67dacaf3cbe1dd30d54cf94a20acfd8c59c9f
Wes Kaminer <WesK@static.local>
2012-04-18 00:38:16 -0400

    added JSON to project 3

---
+++ sdi-project3/json.js
@@ -0,0 +1,41 @@
+// Author: Kirk Kaminer
+// 04/18/2012
+// Assignment 3
+// JSON
+
+var group = {
+	"characters":{
+		"priest":{
+			"role": "healer",
+			"level": "3"
+		},
+		"warrior":{
+			"role": "tank",
+			"level": "6"
+		},
+		"mage":{
+			"role": "aoe",
+			"level": "4"
+		}
+	}
+};
+
+var group2 = {
+	"characters":[
+		{
+			"player": "preist",
+			"role": "healer",
+			"level": "3"
+		},
+		{
+			"player": "warrior",
+			"role": "tank",
+			"level": "6"
+		},
+		{
+			"player": "mage",
+			"role": "aoe",
+			"level": "4"
+		}
+	]
+};
